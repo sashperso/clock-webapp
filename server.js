62b23c3e43360d48e03f9610fc741c1888f83c79
sashperso <personeni.sasha@gmail.com>
2023-10-24 19:01:28 +1100

    fix the missing path

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-// HANDLE TIME ZONE CHANGES
-
 const express = require('express');
-const moment = require('moment-timezone'); // Require the moment-timezone library
+const path = require('path');
+const moment = require('moment-timezone');
 const app = express();
-const port = 3000; // Port number
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'));
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-  
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Define a route to change the time zone
 app.post('/change-timezone', (req, res) => {
-  const { timeZone } = req.body; // Expecting the timeZone as a JSON parameter
-  
-  
+  const { timeZone } = req.body;
+
   if (!timeZone) {
     res.status(400).json({ error: 'Time zone is required.' });
   } else {
     try {
-      // Convert the current time to the specified time zone
       const currentTime = moment().tz(timeZone).format('YYYY-MM-DD HH:mm:ss');
       res.json({ message: `Time zone changed to ${timeZone}`, currentTime });
     } catch (error) {
